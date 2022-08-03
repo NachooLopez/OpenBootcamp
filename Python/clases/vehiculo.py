f520c2916b179834c5fa5084be99d8e76cad61b8
NachooLopez <ignacio.fco.lopez@gmail.com>
2022-08-03 17:45:01 -0300

    finish vehiculo hw

---
+++ Python/clases/vehiculo.py
@@ -0,0 +1,22 @@
+class Vehiculo():
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+
+    def __str__(self):
+        return str(self.__class__) + '\n' + '\n'.join(('{} = {}'.format(item, self.__dict__[item]) for item in self.__dict__))
+
+
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        super().__init__(color, ruedas, puertas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return '\n'.join(('{} = {}'.format(item, self.__dict__[item]) for item in self.__dict__))
+
+
+mi_coche = Coche("rojo", 4, 5, "100km/h", "1.0")
+print(str(mi_coche))
