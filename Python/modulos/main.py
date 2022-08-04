7c2fd4633c2b36d64214e5fed06d3f8cbe6619ef
NachooLopez <ignacio.fco.lopez@gmail.com>
2022-08-04 00:08:58 -0300

    finish 7.1 hw

---
+++ Python/modulos/main.py
@@ -0,0 +1,12 @@
+import calculadora
+
+
+def main():
+    print(calculadora.sumar(20, 30))
+    print(calculadora.restar(20, 30))
+    print(calculadora.multiplicar(20, 30))
+    print(calculadora.dividir(20, 30))
+
+
+if __name__ == "__main__":
+    main()
